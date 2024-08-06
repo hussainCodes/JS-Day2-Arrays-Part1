c6ee646df14e1d5e08e2d595060fcf6099bcded5
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-08-06 10:05:26 +0300

    finish arrays1 tasks

--- arrays1.js
+++ arrays1.js
@@ -1,10 +1,27 @@
-let groceries = ["apple", "banana", "tomato", "pineapple", "passionfruit", "cherry"];
+let groceries = ["apple", "banana", "tommato", "pineapple", "passionfruit", "cherry"];
 console.log(groceries[1]);
 console.log(groceries.length);
 console.log(groceries[groceries.length - 1]);
 let lastFruit = groceries.pop();
-//console.log(lastFruit);
+console.log(lastFruit);
 groceries.push("rasberry", "Avocado");
-console.log(groceries);
+
 let firstThreeItems = groceries.slice(0,3);
-console.log(firstThreeItems);
+
+console.log(groceries);
+function deleteElement(position, list){
+   let firstPart = list.slice(0, position);
+   let secondPart = list.slice(position, list.length);
+   firstPart.pop();
+   groceries = firstPart.concat(secondPart);
+}
+
+deleteElement(3, groceries);
+console.log(groceries);
+
+groceries.unshift("Strawberry")
+console.log(groceries);
+groceries.shift();
+groceries.shift();
+groceries.unshift("ketchaup", "chilli");
+console.log(groceries);
